2e4a7205cc6a0322e23c7ee73ce8d215b9e49a5b
hugsSalazar <hugs.salazar@gmail.com>
2026-06-15 19:16:28 -0600

    fix the inventory attribute

--- Python intermedio/OOP/extras/product.py
+++ Python intermedio/OOP/extras/product.py
@@ -6,7 +6,8 @@ class Product:
 
 
 class Inventory:
-    products = []
+    def __init__(self):
+        self.products = []
 
     def add_product(self, product):
         self.products.append(product)
