2d85c2938d97e0e9e85974d195672e1ee913dca0
hugsSalazar <hugs.salazar@gmail.com>
2026-06-15 19:02:54 -0600

    fix human class parameters

--- Python intermedio/OOP/body.py
+++ Python intermedio/OOP/body.py
@@ -29,7 +29,7 @@ class Feet:
 
 
 class Human:
-    def __init__(self, head, left_arm, right_arm, left_leg, right_leg):
+    def __init__(self, head, torso, left_arm, right_arm, left_leg, right_leg):
         self.head = head
         self.left_arm = left_arm
         self.right_arm = right_arm
